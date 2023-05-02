6797d710d5f86ea139837be4e6e171ff73d7b980
avrO6 <127057199+avrO6@users.noreply.github.com>
2023-05-03 01:23:17 +0200

    Update Ecuacion.vb

--- CalculadoraBBDD/CalculadoraBBDD/Dominio/Ecuacion.vb
+++ CalculadoraBBDD/CalculadoraBBDD/Dominio/Ecuacion.vb
@@ -1,4 +1,6 @@
-﻿Public Class Ecuacion
+Public Class Ecuacion
+
+    'Declaracion de las variables '
 
     Private a As Double
     Private b As Double
@@ -6,9 +8,37 @@
     Private rm1 As Double
     Private im1 As Double
     Private rm2 As Double
-    Private mi2 As Double
+    Private im2 As Double
 
+    'Properties de las variables de entrada'
+    Public Property PropiedadA As Double
+        Get
+            Return a
+        End Get
+        Set(ByVal value As Double)
+            a = value
+        End Set
+    End Property
 
+    Public Property PropiedadB As Double
+        Get
+            Return b
+        End Get
+        Set(ByVal value As Double)
+            b = value
+        End Set
+    End Property
+
+    Public Property PropiedadC As Double
+        Get
+            Return c
+        End Get
+        Set(ByVal value As Double)
+            c = value
+        End Set
+    End Property
+
+    'Constructor de la clase Ecuacion'
     Public Sub New(ByVal a As Double, ByVal b As Double, ByVal c As Double)
         Me.a = a
         Me.b = b
@@ -16,7 +46,6 @@
     End Sub
 
     Function calcular() As Double
-
 
 
         'Calcular el discriminante (la parte de la raiz cuadrada)'
@@ -34,13 +63,15 @@
             rm2 = (-b - Math.Sqrt(discriminante)) / (2 * a)
         Else
             'te dice si la ecuación tiene soluciones imaginarias'
-            rm1 = (-b +) / (2 * a)
-            rm2 = (-b - Math.Sqrt(discriminante)) / (2 * a)
-            im1
-            im2
+            rm1 = -b / (2 * a)
+            rm2 = -b / (2 * a)
+            im1 = Math.Sqrt(-discriminante)
+            im2 = Math.Sqrt(-discriminante)
         End If
 
-        Return Nothing
+        Return discriminante
+
     End Function
 
 End Class
+
