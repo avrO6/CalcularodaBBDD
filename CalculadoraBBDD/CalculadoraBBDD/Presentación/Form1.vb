ece548d512d4a6d16ccad9290f21caec1ca92a5b
avrO6 <127057199+avrO6@users.noreply.github.com>
2023-05-04 17:19:15 +0200

    Update Form1.vb

--- CalculadoraBBDD/CalculadoraBBDD/Presentación/Form1.vb
+++ CalculadoraBBDD/CalculadoraBBDD/Presentación/Form1.vb
@@ -1,17 +1,23 @@
 Public Class Form1
 
-    'Boton de cerrar'
+    'Boton de salir'
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Salir.Click
         Me.Close()
     End Sub
 
 
-
+    'Boton de calcular'
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Calcula.Click
 
-        Dim a, b, c, discriminante As Double
+        Dim a As Double
+        Dim b As Double
+        Dim c As Double
 
-        'Leer los valores que introduces en los cuadros de texto'
+        'Lee el valor que introduces en A y comprueba que no sea 0'
+        If datoA.Text = 0 Then
+            MsgBox("El valor de 'a' no puede ser 0 ")
+        End If
+        'Lee los valores que introduces en los cuadros de texto y comprueba si son letras'
         If IsNumeric(datoA.Text) And IsNumeric(datoB.Text) And IsNumeric(datoC.Text) Then
             a = CDbl(datoA.Text)
             b = CDbl(datoB.Text)
@@ -19,29 +25,30 @@ Public Class Form1
         Else
             'Te muestra un mensaje de error si algún valor no es numérico'
             MsgBox("Ingrese valores numéricos para a, b y c")
+            Exit Sub 'Se sale de la funcion si hay un error'
         End If
 
         'Calcular el discriminante (la parte de la raiz cuadrada)'
         Dim ecuacion = New Ecuacion(a, b, c)
-        discriminante = ecuacion.calcular
+        Dim discriminante As Double = ecuacion.calcular()
         'Comprueba si la ecuación tiene soluciones reales o imaginarias'
         If discriminante > 0 Then
             'Te dice si la ecuación tiene dos soluciones reales'
-            raiz1.Text = ecuacion.rm1.
-            raiz2.Text = ecuacion.rm2
+            raiz1.Text = ecuacion.Rm11
+            raiz2.Text = ecuacion.Rm21
 
             tipo.Text = "Soluciones reales"
             raiz1.ForeColor = Color.Green
         ElseIf discriminante = 0 Then
             'te dice si la ecuación tiene una solución real'
-            raiz1.Text = ecuacion.rm1
-            raiz2.Text = ecuacion.rm2
+            raiz1.Text = ecuacion.Rm11
+            raiz2.Text = ecuacion.Rm21
 
             tipo.Text = "Solución real"
         Else
             'te dice si la ecuación tiene soluciones imaginarias'
-            raiz1.Text = ecuacion.rm1 & " + " & ecuacion.im1 & "i"
-            raiz2.Text = ecuacion.rm2 & " - " & ecuacion.im2 & "i"
+            raiz1.Text = ecuacion.Rm11 & " + " & ecuacion.Im11 & "i"
+            raiz2.Text = ecuacion.Rm21 & " - " & ecuacion.Im21 & "i"
 
             tipo.Text = "Soluciones imaginarias"
         End If
